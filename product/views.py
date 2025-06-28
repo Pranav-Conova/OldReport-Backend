5687f584967ac23bfe6edc39e72bf2c43bb83723
Pranav-Conova <pranavt108@gmail.com>
2025-06-28 20:09:35 +0530

    permission updated for products

--- product/views.py
+++ product/views.py
@@ -1,12 +1,15 @@
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+from api import permissions
 
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsManagerOrReadOnly]
 
 
 class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsManagerOrReadOnly]
