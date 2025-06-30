990d6e885b6155cab61d4c93990b25fc6c11e89c
Pranav-Conova <pranavt108@gmail.com>
2025-06-30 21:04:05 +0530

    stringfy json issue4

--- product/views.py
+++ product/views.py
@@ -5,6 +5,7 @@ from .models import Product
 from .serializers import ProductSerializer
 from api.permissions import IsManagerOrReadOnly
 import json
+from.models import Product,ProductImage,ProductStock
 
 
 class ProductListCreateView(APIView):
@@ -17,17 +18,44 @@ class ProductListCreateView(APIView):
 
     def post(self, request):
         data = request.data.copy()
-        stock_details = data.get('stock_details')
-        if isinstance(stock_details, str):
-            try:
-                data['stock_details'] = json.loads(stock_details)
-            except Exception:
-                return Response({"stock_details": "Invalid JSON."}, status=status.HTTP_400_BAD_REQUEST)
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        images = request.FILES.getlist('images')
+        
+        # Parse stock_details JSON
+        stock_details = json.loads(data.get('stock_details', '[]'))
+        
+        # Create Product
+        product = Product.objects.create(
+            name=data.get('name'),
+            description=data.get('description'),
+            price=data.get('price'),
+            category=data.get('category'),
+            subcategory=data.get('subcategory'),
+            bestseller=data.get('bestseller') == 'true'
+        )
+        for stock in stock_details:
+            ProductStock.objects.create(
+                product=product,
+                size=stock['size'],
+                quantity=stock['quantity']
+            )
+        
+        # Create ProductImage entries
+        for image in images:
+            ProductImage.objects.create(
+                product=product,
+                image=image
+            )
+        # stock_details = data.get("stock_details")
+        # if isinstance(stock_details, str):
+        #     try:
+        #         data['stock_details'] = json.loads(stock_details)
+        #     except Exception:
+        #         return Response({"stock_details": "Invalid JSON."}, status=status.HTTP_400_BAD_REQUEST)
+        # serializer = ProductSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        return Response("created", status=status.HTTP_201_CREATED)
+        return Response("error", status=status.HTTP_400_BAD_REQUEST)
 
 class ProductDetailView(APIView):
     # permission_classes = [IsManagerOrReadOnly]
