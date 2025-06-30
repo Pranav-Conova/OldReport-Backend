19131897ea790c4645cd9f35db9ff3c443a30824
Pranav-Conova <pranavt108@gmail.com>
2025-06-30 18:18:59 +0530

    detailed product view

--- product/urls.py
+++ product/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import ProductListCreateView, ProductDeleteView
+from .views import ProductListCreateView, ProductDeleteView,ProductDetailView
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('products/', ProductListCreateView.as_view(), name='product-list-create'),
-    path('products/<int:pk>/', ProductDeleteView.as_view(), name='product-delete'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product-delete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- product/views.py
+++ product/views.py
@@ -11,6 +11,7 @@ class ProductListCreateView(APIView):
         products = Product.objects.prefetch_related('stock_details', 'images').all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+    
 
     def post(self, request):
         serializer = ProductSerializer(data=request.data)
@@ -19,7 +20,18 @@ class ProductListCreateView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class ProductDetailView(APIView):
+    # permission_classes = [IsManagerOrReadOnly]
 
+    def get(self, request, pk):
+        try:
+            product = Product.objects.prefetch_related('stock_details', 'images').get(pk=pk)
+        except Product.DoesNotExist:
+            return Response({"detail": "Product not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
 class ProductDeleteView(APIView):
     # permission_classes = [IsManagerOrReadOnly]
     def delete(self, request, pk):
