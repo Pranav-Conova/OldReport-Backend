16cd5acd36b43fc71ee8943d03cff10697a6afa9
Pranav-Conova <pt0191661@gmail.com>
2025-09-09 21:31:41 +0530

    lastname changed

--- api/models.py
+++ api/models.py
@@ -9,10 +9,12 @@ class CustomUser(AbstractUser):
         ('manager', 'Manager'),
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='client')
+    # make last_name nullable/optional at the model level
+    last_name = models.CharField(max_length=150, blank=True, null=True)
     
     def __str__(self):
         return self.first_name
-    pass 
+    
 
 class Address(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='address')
